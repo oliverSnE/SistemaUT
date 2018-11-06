c19e51dddc9dcc8fabe07a34d2240a3224c68a93
OliverSnE Burgara <sinue_oliver@hotmail.com>
2018-11-05 19:44:53 -0700

    creando AccountController

---
+++ SistemaUTH/SistemaUTH/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaUTH.Controllers
+{
+    public class AccountController : Controller
+    {
+        //TODO: Hacer countController para los roles.
+        private readonly UserManager<user> _userManager;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
